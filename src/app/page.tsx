8ab4f91e4f1ae821397cbf040e4072caaa59373b
Rodrigo Sakae <devsakae@gmail.com>
2023-06-01 15:59:49 -0300

    feat: google analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import About from "./components/About";
 import Documents from "./components/Documents";
 import Footer from "./components/Footer";
@@ -8,6 +9,20 @@ import Projects from "./components/Projects";
 export default function Home() {
   return (
     <>
+    {/* Google tag (gtag.js) */}
+    <Script
+      src="https://www.googletagmanager.com/gtag/js?id=G-HCQ6KVJ685"
+      strategy="afterInteractive"
+    />
+    <Script id="google-analytics" strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-HCQ6KVJ685');
+      `}
+    </Script>
       <main>
         <Hero />
         <About />
