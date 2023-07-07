8d744666b79c10803026736b6fae2d8774f6ed4f
BradockBr <robtognetti@gmail.com>
2023-07-07 13:37:04 -0300

    buttons name in mouseclick

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 (function () {
-    [...document.querySelectorAll(".control")].forEach(button => {
-        button.addEventListener("click", function() {
-            document.querySelector(".active-btn").classList.remove("active-btn");
-            this.classList.add("active-btn");
-            document.querySelector(".active").classList.remove("active");
-            document.getElementById(button.dataset.id).classList.add("active");
-        })
+    var controls = document.querySelector(".controls");
+    var controlsItems = controls.querySelectorAll(".control");
+    var textoDinamico = document.getElementById("texto-dinamico");
+  
+    controlsItems.forEach(function (button) {
+      button.addEventListener("mouseenter", function () {
+        var texto = button.getAttribute("data-text");
+        textoDinamico.textContent = texto;
+        textoDinamico.style.display = "block";
+      });
+  
+      button.addEventListener("mouseleave", function () {
+        textoDinamico.style.display = "none";
+      });
     });
-    document.querySelector(".theme-btn").addEventListener("click", () => {
-        document.body.classList.toggle("light-mode");
-    })
-})();
+  
+    controls.addEventListener("click", function (event) {
+      var clickedBtn = event.target.closest(".control");
+      if (clickedBtn) {
+        controlsItems.forEach(function (button) {
+          button.classList.remove("active-btn");
+        });
+        clickedBtn.classList.add("active-btn");
+        var id = clickedBtn.getAttribute("data-id");
+        var activeContent = document.getElementById(id);
+        var activeContents = document.querySelectorAll(".active");
+        activeContents.forEach(function (content) {
+          content.classList.remove("active");
+        });
+        activeContent.classList.add("active");
+      }
+    });
+  
+    document.querySelector(".theme-btn").addEventListener("click", function () {
+      document.body.classList.toggle("light-mode");
+    });
+  })();
+  
